eae7f44d3a520fddd8a3607e2fdc8842ff03c2d5
SheebaN-Python <67795028+SheebaN-Python@users.noreply.github.com>
2020-07-03 23:41:41 +0530

    Initial Commit

---
+++ The-Living-World.py
@@ -0,0 +1,65 @@
+score=0
+# QUESTION 1
+answer1=input("Which of the following is not a result of cell division? \na. Growth \nb. Repair \nc. Metabolism \nd. Reproduction \nAnswer:")
+if answer1 == "a" or answer1== "Growth":
+    score += 1
+    print("Correct!")
+    print ("score: ", score)
+    print("\n")
+    
+else:
+    print("Incorrect! The Correct answer is Growth.")
+    print("Score: ", score)
+    print("\n")
+
+# QUESTION 2
+answer2=input("Mark the incorrect Pair. \na. Hydra - Budding \nb. Flatworm - Regeneration \nc. Amoeba - Fragmentation \nd. Yeast - Budding \nAnswer:")
+if answer2 == "c" or answer2== "Amoeba - Fragmentation":
+    score += 1
+    print("Correct!")
+    print ("score: ", score)
+    print("\n")
+    
+else:
+    print("Incorrect! The Correct answer is Amoeba - Fragmentation.")
+    print("Score: ", score)
+    print("\n")
+
+# QUESTION 3
+answer3=input("Which of the following is incorrect for reproduction? \na. Unicellular organism reproduce by cell division \nb. Reproduction is a characteristic of all living organisms \nc. In unicellular organism, reproduction and growth are linked together \nd. Non-living objects are incapable of reproducing \nAnswer:")
+if answer3 == "b" or answer3 == "Reproduction is a characteristic of all living organisms  ":
+    score += 1
+    print("Correct!")
+    print ("score: ", score)
+    print("\n")
+    
+else:
+    print("Incorrect! The Correct answer is Reproduction is a characteristic of all living organisms.")
+    print("Score: ", score)
+    print("\n")
+
+# QUESTION 4
+answer4=input("Mark the incorrect statement w.r.t. metabolism. \na. Microbes exhibit the metabolism \nb. It is the property of all living forms \nc. The metabolic reactions can be demonstrated in-vitro \nd. It is not a defining feature of life forms \nAnswer:")
+if answer4 == "d" or answer4 == "It is not a defining feature of life forms ":
+    score += 1
+    print("Correct!")
+    print ("score: ", score)
+    print("\n")
+    
+else:
+    print("Incorrect! The Correct answer is It is not a defining feature of life forms.")
+    print("Score: ", score)
+    print("\n")
+
+# QUESTION 5
+answer5=input("Non-living objects exhibit/show \na. Property of self-replication \nb. Evolution \nc. Self-regulating interactive systems \nd. Reversible growth \nAnswer:")
+if answer5 == "d" or answer5== "Reversible growth":
+    score += 1
+    print("Correct!")
+    print ("score: ", score)
+    print("\n")
+    
+else:
+    print("Incorrect! The Correct answer is Reversible growth.")
+    print("Score: ", score)
+    print("\n")
